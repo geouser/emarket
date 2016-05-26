7411a134b13fae84e1c775dc338846e4824aadec
Serhiy <serhiyhulyi93@gmail.com>
2016-05-26 12:52:08 +0300

    yandex map

--- js/main.js
+++ js/main.js
@@ -132,53 +132,25 @@ $('.historySlider__slide .button').click(function(e){
   /*---------------------------
                               GOOGLE MAP
 ---------------------------*/
-  var map;
-  function googleMap_initialize() {
-    var lat = $('#map_canvas').data('lat');
-    var long = $('#map_canvas').data('long');
-    var mapCenterCoord = new google.maps.LatLng(lat+0.001, long+0.006);
-    var mapMarkerCoord = new google.maps.LatLng(lat, long);
-    if ( $(window).width() < 768 ) {
-      mapCenterCoord = new google.maps.LatLng(lat, long);
-      mapMarkerCoord = new google.maps.LatLng(lat, long);
-    }
-    $(window).resize(function(event) {
-      if ( $(window).width() < 768 ) {
-        mapCenterCoord = new google.maps.LatLng(lat, long);
-        mapMarkerCoord = new google.maps.LatLng(lat, long);
-      } else {
-        mapCenterCoord = new google.maps.LatLng(lat+0.001, long+0.006);
-        mapMarkerCoord = new google.maps.LatLng(lat, long);
-      }
-    });
-
-    var mapOptions = {
-      center: mapCenterCoord,
-      zoom: 15,
-      //draggable: false,
-      disableDefaultUI: false,
-      scrollwheel: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    var markerImage = new google.maps.MarkerImage('images/location.png');
-    var marker = new google.maps.Marker({
-      icon: markerImage,
-      position: mapMarkerCoord, 
-      map: map,
-      title:"MegaExpress"
-    });
-    
-    $(window).resize(function (){
-      map.setCenter(mapCenterCoord);
-    });
-  }
   if ( $('#map_canvas').length > 0) {
-    googleMap_initialize();   
+    var myMap;
+    coord = [59.934303, 30.337670];
+    ymaps.ready(function () {
+        myMap = new ymaps.Map("map_canvas", {
+            center: coord,
+            zoom: 17,
+            controls: []
+        });
+
+        var placemark = new ymaps.Placemark(coord, null, {
+            iconLayout: 'default#image',
+            iconImageHref: 'images/map-marker.png',
+            iconImageSize: [86, 74],
+            iconImageOffset: [-24, -74],
+        });
+        myMap.geoObjects.add(placemark);
+    }); 
   }
-
-
 
 }); // end
 
